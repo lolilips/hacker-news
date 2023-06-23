305e6ddacdde4f8f9d21f00cf5004cba8fb39cd1
lolilips <werewolf.00@mail.ru>
2023-06-23 19:09:40 +0300

    Added hacker-news api

--- src/NewsItem/NewsItem.js
+++ src/NewsItem/NewsItem.js
@@ -1,15 +1,23 @@
 import style from './NewsItem.module.css'
+import {unixToDate} from "../utils/utils";
 export function NewsItem(props){
-
+    const  scoreClassArr = [style.score]
+if (props.score < 50){
+scoreClassArr.push(style.highScore)
+}else if(props.score>30){
+    scoreClassArr.push((style.lowScore))
+}else{
+    scoreClassArr.push(style.midScore)
+}
     return(
         <div className={style.container}>
         <a className={style.link} href="example.com">{props.title}</a>
 <div className={style.info}>
     <div className={style.userDate}>
     <span>{props.username}</span>
-    <span>{props.date}</span>
+    <span>{unixToDate(props.date)}</span>
 </div>
-            <div className={style.score}>
+            <div className={scoreClassArr.join(' ')}>
                 {props.score} points
             </div>
 </div>
