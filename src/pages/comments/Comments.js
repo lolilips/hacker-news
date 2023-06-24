ac2b796a771a949c3e397b96ac72def284e95e79
lolilips <werewolf.00@mail.ru>
2023-06-24 23:46:14 +0300

    Added info about news

--- src/pages/comments/Comments.js
+++ src/pages/comments/Comments.js
@@ -3,13 +3,25 @@ import {NewsItem} from "../../components/NewsItem/NewsItem";
 import {useEffect, useState} from "react";
 import {get} from "../../api/api";
 
-export function Comments(){
+export function Comments() {
     const {id} = useParams()
-    const [news,setNews] = useState()
-    async function getNewsDate(newsId){
+    const [news, setNews] = useState()
+    const [comments, setComments] = useState([])
+
+    async function getNewsDate(newsId) {
         const newsDate = await get(`https://hacker-news.firebaseio.com/v0/item/${newsId}.json?print=pretty`)
-    setNews(newsDate)
+        setNews(newsDate)
+        if (newsDate?.kids) {
+            getNewsCommet(newsDate.kids)
     }
+}
+    async function getNewsCommet(commentIds) {
+        const commentsData = await Promise
+            .all(commentIds.map(commentIds => get(`https://hacker-news.firebaseio.com/v0/item/${commentIds}.json?print=pretty`)))
+   setComments(commentsData)
+        console.log(commentsData)
+    }
+
     useEffect(() => {
         getNewsDate(id)
     }, [id])
